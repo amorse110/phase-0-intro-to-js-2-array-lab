0500b5d9cc85608b26dd0c9ef8bba3b9d03ac79c
Austin Morse <amorse110@gmail.com>
2023-05-23 09:32:04 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const allCats = [...cats, "Broom"];
+    return allCats
+}
+
+function prependCat(name) {
+    const allCats2 = ["Arnold", ...cats];
+    return allCats2
+}
+
+function removeLastCat() {
+    const allCats3 = cats.slice(0, 2)
+    return allCats3
+}
+
+function removeFirstCat() {
+    const allCats4 = cats.slice(1)
+    return allCats4
+}
